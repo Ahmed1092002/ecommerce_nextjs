0a58d80c4a0ebfb799d730342ad846f56b51d1e0
Ahmed Tamer Elsayed <112315071+Ahmed1092002@users.noreply.github.com>
2025-12-08 19:34:39 +0200

    Refactor code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["dvukibpermcdulmzzujf.supabase.co", "127.0.0.1", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dvukibpermcdulmzzujf.supabase.co",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
 };
 
